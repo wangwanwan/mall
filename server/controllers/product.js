75dbd5778fec690b140ace43d6ee793171e164bd
wangwan <wangwan0428@163.com>
2018-09-12 23:17:22 +0800

    part-1,add detail page

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -2,5 +2,14 @@ const DB = require('../utils/db.js');
 module.exports = {
   list: async ctx => {
     ctx.state.data = await DB.query("SELECT * FROM product;");
+  },
+
+  detail: async ctx => {
+    var productId = + ctx.params.id;
+    if (!isNaN(productId)) {
+      ctx.state.data = (await DB.query('select * from product where product.id = ?', [productId]))[0];
+    } else {
+      ctx.state.data = {};
+    }
   }
 }
--- server/routes/index.js
+++ server/routes/index.js
@@ -34,4 +34,6 @@ router.post('/message', controllers.message.post)
 
 router.get('/product', controllers.product.list)
 
+router.get('/product/:id', controllers.product.detail)
+
 module.exports = router
